2777935d32980604aef6a2e9c12fdd5df903bd4a
Martins Anerua <aneruamartins@gmail.com>
2023-02-10 18:26:32 +0100

    feat: add country model

--- academics/models.py
+++ academics/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from helpers.models import TrackingModel
+
+
+class Country(TrackingModel):
+
+    name = models.CharField(_("name of country"), max_length=255)
+    description = models.TextField(_("Description"))
