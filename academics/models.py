39ec333498c760543a142fbfe924e9fd325ece26
Martins Anerua <aneruamartins@gmail.com>
2023-02-10 18:30:53 +0100

    feat: add school model

--- academics/models.py
+++ academics/models.py
@@ -6,5 +6,13 @@ from helpers.models import TrackingModel
 
 class Country(TrackingModel):
 
-    name = models.CharField(_("name of country"), max_length=255)
+    name = models.CharField(_("Name of country"), max_length=255)
     description = models.TextField(_("Description"))
+
+
+class School(TrackingModel):
+
+    name = models.CharField(_("Name of school"), max_length=255)
+    description = models.TextField(_("Description"))
+    address = models.CharField(_("Address of school, excluding its country"), max_length=255)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
