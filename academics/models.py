47f4f971ebadf2194f8ce9d3d3afe8f8ab5781c6
Martins Anerua <aneruamartins@gmail.com>
2023-03-11 18:07:52 +0100

    feat: add fields to course, create new models

--- academics/models.py
+++ academics/models.py
@@ -27,6 +27,7 @@ class Country(TrackingModel):
     population = models.IntegerField(_("Population of country"))
     students = models.IntegerField(_("Total number of students in country"), blank=True, null=True)
     international_students = models.IntegerField(_("Total number of international students in country"), blank=True, null=True)
+    currency = models.ForeignKey('Currency', on_delete=models.CASCADE, related_name="currency_countries")
     
     about = models.TextField(_("About"))
     about_wiki_link = models.URLField(_("Link to about on Wikipedia"))
@@ -49,23 +50,47 @@ class School(TrackingModel):
     students = models.IntegerField(_("Total number of students in the school"), blank=True, null=True)
 
 
-
 class Discipline(TrackingModel):
 
     name = models.CharField(_("Name of discipline"), max_length=255)
-    description = models.TextField(_("Description"))
+    about = models.TextField(_("About"))
+
+
+class Currency(TrackingModel):
+
+    name = models.CharField(_("Name of currency"), max_length=255)
+    short_code = models.CharField(_("Short code of the currency"), max_length=5)
+    usd_exchange_rate = models.DecimalField(_("Exchange rate with the USD"))
+
+
+class ProgrammeType(TrackingModel):
+
+    name = models.CharField(_("Name of programme type, e.g. Bachelors"), max_length=255)
+    about = models.TextField(_("About"))
+
+
+class DegreeType(TrackingModel):
+
+    name = models.CharField(_("Name of degree type, e.g. Bachelor of Science"), max_length=255)
+    short_name = models.CharField(_("Short name of degree type, e.g. B.Sc."), max_length=255)
+    programme_type = models.ForeignKey(ProgrammeType, on_delete=models.CASCADE, related_name="programme_type_degree_types")
+
+
+class CourseDates(TrackingModel):
+
+    start_date_year = models.IntegerField(_("Start date year"), blank=True, null=True)
+    start_date_month = models.IntegerField(_("Start date month"), blank=True, null=True)
+    application_deadline_year = models.IntegerField(_("Application deadline year"), blank=True, null=True)
+    application_deadline_month = models.IntegerField(_("Application deadline month"), blank=True, null=True)
+
+
+class Language(TrackingModel):
+
+    name = models.CharField(_("Name of language"), max_length=255)
+    iso_639_code = models.CharField(_("ISO 639 two-letter abbreviation"), max_length=2)
 
 
 class Course(TrackingModel):
-
-    DOLLAR = "USD"
-    EURO = "EUR"
-    POUND = "BPD"
-    CURRENCY_CHOICES = (
-        (DOLLAR, _("DOLLAR")),
-        (EURO, _("EURO")),
-        (POUND, _("POUND")),
-    )
 
     FULL_TIME = "FULL"
     PART_TIME = "PART"
@@ -75,41 +100,36 @@ class Course(TrackingModel):
     )
 
     ON_SITE = "SITE"
-    REMOTE = "REMOTE"
+    ONLINE = "ONLINE"
+    BLENDED = "BLENDED"
     COURSE_ATTENDANCE_CHOICES = (
         (ON_SITE, _("ON SITE")),
-        (REMOTE, _("REMOTE")),
-    )
-
-    BACHELORS = "BSC"
-    MASTERS = "MSC"
-    DOCTORATE = "PHD"
-    DIPLOMA = "PGD"
-    COURSE_PROGRAMME_CHOICES = (
-        (BACHELORS, _("BACHELORS")),
-        (MASTERS, _("MASTERS")),
-        (DOCTORATE, _("DOCTORATE")),
-        (DIPLOMA, _("DIPLOMA")),
+        (ONLINE, _("ONLINE")),
+        (BLENDED, _("BLENDED")),
     )
 
     name = models.CharField(_("Name of school"), max_length=255)
-    description = models.CharField(_("Description"), max_length=1024)
-    overview = models.TextField(_("Description"))
+    about = models.CharField(_("About"), max_length=1024, blank=True)
+    overview = models.TextField(_("Overview"), blank=True)
 
     duration = models.IntegerField(_("Duration (in months) of course"), blank=True)
-    start_date_year = models.IntegerField(_("Start date year"), blank=True, null=True)
-    start_date_month = models.CharField(_("Start date month"), max_length=3, blank=True)
-    application_deadline_year = models.IntegerField(_("Application deadline year"), blank=True, null=True)
-    application_deadline_month = models.CharField(_("Application deadline month"), max_length=3, blank=True)
+    course_dates = models.ManyToManyField(CourseDates, related_name="course_dates_courses", blank=True, null=True)
 
     school = models.ForeignKey(School, on_delete=models.CASCADE, related_name="school_courses")
     disciplines = models.ManyToManyField(Discipline, related_name="discipline_courses")
 
     tuition_fee = models.IntegerField(_("Tuition fee"), blank=True, null=True)
-    tuition_currency = models.CharField(_("Tuition fee currency"), max_length=3, choices=CURRENCY_CHOICES, blank=True)
+    tuition_fee_base = models.CharField(_("Tuition fee base e.g. year, semester, full"), blank=True)
+    tuition_currency = models.ForeignKey(Currency, on_delete=models.CASCADE, related_name="currency_courses", blank=True, null=True)
 
     format = models.CharField(_("Course format"), max_length=4, choices=COURSE_FORMAT_CHOICES, blank=True)
     attendance = models.CharField(_("Course attendance format"), max_length=6, choices=COURSE_ATTENDANCE_CHOICES, blank=True)
-    programme = models.CharField(_("Course programme"), max_length=10, choices=COURSE_PROGRAMME_CHOICES)
-    #degree_type
-    #language
+    programme_type = models.ForeignKey(ProgrammeType, on_delete=models.CASCADE, related_name="programme_type_courses")
+    degree_type = models.ForeignKey(DegreeType, on_delete=models.CASCADE, related_name="degree_type_courses")
+
+    language = models.ForeignKey(Language, on_delete=models.CASCADE, related_name="language_courses")
+
+    programme_structure = models.TextField(_("Programme structure"), blank=True)
+    admission_requirements = models.TextField(_("Admission requirements"), blank=True)
+    
+    official_programme_website = models.URLField(_("Official programme website"), blank=True)
