59e5eb0a3e3de54c4dec1b1f7647c532ff178cdc
Martins Anerua <aneruamartins@gmail.com>
2023-03-13 09:51:06 +0100

    feat: register models on admin site

--- academics/admin.py
+++ academics/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from academics.models import Country, School, Discipline, Currency, ProgrammeType, DegreeType, CourseDates, Language, Course
+
+
+admin.site.register(Country)
+admin.site.register(School)
+admin.site.register(Discipline)
+admin.site.register(Currency)
+admin.site.register(ProgrammeType)
+admin.site.register(DegreeType)
+admin.site.register(CourseDates)
+admin.site.register(Language)
+admin.site.register(Course)
