ad919ce65ce7320d79d3f86de0e4a4588e7876a2
Martins Anerua <aneruamartins@gmail.com>
2023-03-21 09:06:05 +0100

    fix: include corsheaders middleware

--- hillpad/settings.py
+++ hillpad/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS").split(" ")
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
