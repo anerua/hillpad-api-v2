219771ee12da5844902329e0226c0470b555ac16
Martins Anerua <aneruamartins@gmail.com>
2023-03-23 13:59:35 +0100

    feat: add domain verification text file

--- hillpad/urls.py
+++ hillpad/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/account/', include("account.urls")),
     path('api/academics/', include("academics.urls")),
+    path('.well-known/pki-validation/529534C77577221035E87408FF697AE9.txt', TemplateView.as_view(template_name="529534C77577221035E87408FF697AE9.txt", content_type="text/plain")),
     path('', include("frontend.urls")),
 ]
