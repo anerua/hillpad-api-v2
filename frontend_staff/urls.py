336473b3478d1ce545d2acfa65f1264598a8df78
Martins Anerua <aneruamartins@gmail.com>
2023-04-12 16:59:39 +0100

    feat: staff course list, edit and create pages

--- frontend_staff/urls.py
+++ frontend_staff/urls.py
@@ -5,4 +5,7 @@ from frontend_staff import views
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="staff_home"),
+    path("courses", views.CoursesListingView.as_view(), name="staff_courses_listing"),
+    path("course/create", views.CourseCreateView.as_view(), name="staff_course_create"),
+    path("course/<int:id>", views.CourseDetailView.as_view(), name="staff_course_detail"),
 ]
--- frontend_staff/views.py
+++ frontend_staff/views.py
@@ -4,3 +4,18 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
 
     template_name = "frontend_staff/index.html"
+
+
+class CoursesListingView(TemplateView):
+
+    template_name = "frontend_staff/course/list.html"
+
+
+class CourseCreateView(TemplateView):
+
+    template_name = "frontend_staff/course/create.html"
+
+
+class CourseDetailView(TemplateView):
+
+    template_name = "frontend_staff/course/edit.html"
