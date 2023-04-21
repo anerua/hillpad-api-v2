68d17cd4fb8075ec56789784a5302c330537f90f
Martins Anerua <aneruamartins@gmail.com>
2023-04-21 17:44:07 +0100

    feat: add school, prog and lang filters to courses

--- academics/filters.py
+++ academics/filters.py
@@ -9,7 +9,10 @@ class CourseFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
     country = django_filters.CharFilter(field_name='school__country__short_code', lookup_expr='iexact')
     continent = django_filters.CharFilter(field_name='school__country__continent', lookup_expr='iexact')
+    school = django_filters.CharFilter(field_name='school__name', lookup_expr='iexact')
+    programme_type = django_filters.CharFilter(field_name='programme_type__name', lookup_expr='iexact')
+    language = django_filters.CharFilter(field_name='language__iso_639_code', lookup_expr='iexact')
 
     class Meta:
         model = Course
-        fields = ("id", "name", "school")
+        fields = ("id", "name", "school", "programme_type", "language")
