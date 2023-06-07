c0085cea1518786baf3f5d78b74d297ece21d2bf
Martins Anerua <aneruamartins@gmail.com>
2023-06-07 11:54:40 +0100

    feat: add approve, reject and publish school urls

--- academics/urls/school.py
+++ academics/urls/school.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path("detail/<int:pk>", views.DetailSchoolAPIView.as_view(), name="detail_school"),
     path("update/<int:pk>", views.UpdateSchoolAPIView.as_view(), name="update_school"),
     path("delete/<int:pk>", views.DeleteSchoolAPIView.as_view(), name="delete_school"),
+    path("approve/<int:pk>", views.ApproveSchoolAPIView.as_view(), name="approve_course"),
+    path("reject/<int:pk>", views.RejectSchoolAPIView.as_view(), name="reject_course"),
+    path("publish/<int:pk>", views.PublishSchoolAPIView.as_view(), name="publish_course"),
+]
 ]
