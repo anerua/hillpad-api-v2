7c3d43b54d45ce814989678a9b2288e071fd43bc
Martins Anerua <aneruamartins@gmail.com>
2023-06-08 09:16:41 +0100

    feat: add published field to Language model

--- academics/models.py
+++ academics/models.py
@@ -122,6 +122,8 @@ class Language(TrackingModel):
     name = models.CharField(_("Name of language"), max_length=255)
     iso_639_code = models.CharField(_("ISO 639 two-letter abbreviation"), max_length=2)
 
+    published = models.BooleanField(_("Published status of language"), default=False)
+
 
 class Course(TrackingModel):
 
