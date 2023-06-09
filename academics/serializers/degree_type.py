99c6e978ce1b957d53436f0b4d7dae67b03d286a
Martins Anerua <aneruamartins@gmail.com>
2023-06-09 09:09:06 +0100

    feat: update degree type serializer

--- academics/serializers/degree_type.py
+++ academics/serializers/degree_type.py
@@ -20,6 +20,7 @@ class ListDegreeTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = DegreeType
         fields = '__all__'
+        depth = 2
 
 
 class DetailDegreeTypeSerializer(serializers.ModelSerializer):
@@ -27,6 +28,7 @@ class DetailDegreeTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = DegreeType
         fields = '__all__'
+        depth = 2
 
 
 class UpdateDegreeTypeSerializer(serializers.ModelSerializer):
