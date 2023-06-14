96d891dc3aa50ff687f70a895fca6079c3fe3e80
Martins Anerua <aneruamartins@gmail.com>
2023-06-14 15:41:02 +0100

    feat: add detail countrydraft api url

--- academics/urls/country.py
+++ academics/urls/country.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("list", views.ListCountryAPIView.as_view(), name="list_country"),
     path("list_draft", views.ListCountryDraftAPIView.as_view(), name="list_country_draft"),
     path("detail/<int:pk>", views.DetailCountryAPIView.as_view(), name="detail_country"),
+    path("detail_draft/<int:pk>", views.DetailCountryDraftAPIView.as_view(), name="detail_country_draft"),
     path("update/<int:pk>", views.UpdateCountryAPIView.as_view(), name="update_country"),
     path("delete/<int:pk>", views.DeleteCountryAPIView.as_view(), name="delete_country"),
     path("publish/<int:pk>", views.PublishCountryAPIView.as_view(), name="publish_country"),
