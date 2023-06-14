147d64822d8f8bb9051f083102e51d019a45154e
Martins Anerua <aneruamartins@gmail.com>
2023-06-14 14:18:56 +0100

    feat: add list countrydraft url

--- academics/urls/country.py
+++ academics/urls/country.py
@@ -4,8 +4,9 @@ from academics import views
 
 
 urlpatterns = [
-    path("create_draft", views.CreateDraftCountryAPIView.as_view(), name="create_country_draft"),
+    path("create_draft", views.CreateCountryDraftAPIView.as_view(), name="create_country_draft"),
     path("list", views.ListCountryAPIView.as_view(), name="list_country"),
+    path("list_draft", views.ListCountryDraftAPIView.as_view(), name="list_country_draft"),
     path("detail/<int:pk>", views.DetailCountryAPIView.as_view(), name="detail_country"),
     path("update/<int:pk>", views.UpdateCountryAPIView.as_view(), name="update_country"),
     path("delete/<int:pk>", views.DeleteCountryAPIView.as_view(), name="delete_country"),
