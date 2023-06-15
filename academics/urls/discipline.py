0a08f24df51b0c8c4e5862da79b7be4945879286
Martins Anerua <aneruamartins@gmail.com>
2023-06-15 12:55:26 +0100

    feat: add list disciplinedraft url

--- academics/urls/discipline.py
+++ academics/urls/discipline.py
@@ -6,6 +6,7 @@ from academics import views
 urlpatterns = [
     path("create_draft", views.CreateDisciplineDraftAPIView.as_view(), name="create_discipline_draft"),
     path("list", views.ListDisciplineAPIView.as_view(), name="list_discipline"),
+    path("list_draft", views.ListDisciplineDraftAPIView.as_view(), name="list_discipline_draft"),
     path("detail/<int:pk>", views.DetailDisciplineAPIView.as_view(), name="detail_discipline"),
     path("update/<int:pk>", views.UpdateDisciplineAPIView.as_view(), name="update_discipline"),
     path("delete/<int:pk>", views.DeleteDisciplineAPIView.as_view(), name="delete_discipline"),
