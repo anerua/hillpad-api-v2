5c0c9418f65acf789a5daa89dc7ba65aa5340295
Martins Anerua <aneruamartins@gmail.com>
2023-06-15 13:06:56 +0100

    feat: add update disciplinedraft url

--- academics/urls/discipline.py
+++ academics/urls/discipline.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("list_draft", views.ListDisciplineDraftAPIView.as_view(), name="list_discipline_draft"),
     path("detail/<int:pk>", views.DetailDisciplineAPIView.as_view(), name="detail_discipline"),
     path("detail_draft/<int:pk>", views.DetailDisciplineDraftAPIView.as_view(), name="detail_discipline_draft"),
-    path("update/<int:pk>", views.UpdateDisciplineAPIView.as_view(), name="update_discipline"),
+    path("update_draft/<int:pk>", views.UpdateDisciplineDraftAPIView.as_view(), name="update_discipline_draft"),
     path("delete/<int:pk>", views.DeleteDisciplineAPIView.as_view(), name="delete_discipline"),
     path("publish/<int:pk>", views.PublishDisciplineAPIView.as_view(), name="publish_discipline"),
 ]
