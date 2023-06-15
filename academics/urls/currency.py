238af973bb5bc785c21c40717d15e57d491d69c3
Martins Anerua <aneruamartins@gmail.com>
2023-06-15 10:08:12 +0100

    feat: add list currencydraft API url

--- academics/urls/currency.py
+++ academics/urls/currency.py
@@ -6,6 +6,7 @@ from academics import views
 urlpatterns = [
     path("create_draft", views.CreateCurrencyDraftAPIView.as_view(), name="create_currency_draft"),
     path("list", views.ListCurrencyAPIView.as_view(), name="list_currency"),
+    path("list_draft", views.ListCurrencyDraftAPIView.as_view(), name="list_currency_draft"),
     path("detail/<int:pk>", views.DetailCurrencyAPIView.as_view(), name="detail_currency"),
     path("update/<int:pk>", views.UpdateCurrencyAPIView.as_view(), name="update_currency"),
     path("delete/<int:pk>", views.DeleteCurrencyAPIView.as_view(), name="delete_currency"),
