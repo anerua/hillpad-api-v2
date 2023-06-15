472b87a7ffe15a7b20be51483e29e3aa5554b913
Martins Anerua <aneruamartins@gmail.com>
2023-06-15 15:38:18 +0100

    feat: add submit languagedraft url

--- academics/urls/language.py
+++ academics/urls/language.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("detail/<int:pk>", views.DetailLanguageAPIView.as_view(), name="detail_language"),
     path("detail_draft/<int:pk>", views.DetailLanguageDraftAPIView.as_view(), name="detail_language_draft"),
     path("update_draft/<int:pk>", views.UpdateLanguageDraftAPIView.as_view(), name="update_language_draft"),
+    path("submit_draft/<int:pk>", views.SubmitLanguageDraftAPIView.as_view(), name="submit_language_draft"),
     path("delete/<int:pk>", views.DeleteLanguageAPIView.as_view(), name="delete_language"),
     path("publish/<int:pk>", views.PublishLanguageAPIView.as_view(), name="publish_language"),
 ]
