ac120b7cf990862f7b92b0ddd2fc9c37738155df
Martins Anerua <aneruamartins@gmail.com>
2023-06-15 13:54:50 +0100

    feat: add list languagedraft url

--- academics/urls/language.py
+++ academics/urls/language.py
@@ -6,6 +6,7 @@ from academics import views
 urlpatterns = [
     path("create_draft", views.CreateLanguageDraftAPIView.as_view(), name="create_language_draft"),
     path("list", views.ListLanguageAPIView.as_view(), name="list_language"),
+    path("list_draft", views.ListLanguageDraftAPIView.as_view(), name="list_language_draft"),
     path("detail/<int:pk>", views.DetailLanguageAPIView.as_view(), name="detail_language"),
     path("update/<int:pk>", views.UpdateLanguageAPIView.as_view(), name="update_language"),
     path("delete/<int:pk>", views.DeleteLanguageAPIView.as_view(), name="delete_language"),
