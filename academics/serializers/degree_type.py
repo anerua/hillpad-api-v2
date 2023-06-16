b3d3283344412a5ebc15e58b89a4eda365abe721
Martins Anerua <aneruamartins@gmail.com>
2023-06-16 15:29:04 +0100

    feat: add create degreetypedraft serializer

--- academics/serializers/degree_type.py
+++ academics/serializers/degree_type.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from academics.models import DegreeType
+from academics.models import DegreeType, DegreeTypeDraft
 
 
 class CreateDegreeTypeSerializer(serializers.ModelSerializer):
@@ -12,7 +12,31 @@ class CreateDegreeTypeSerializer(serializers.ModelSerializer):
             "name",
             "short_name",
             "programme_type",
+            "author",
+            "degree_type_draft",
+            "published",
         )
+
+
+class CreateDegreeTypeDraftSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = DegreeTypeDraft
+        fields = (
+            "id",
+            "name",
+            "short_name",
+            "programme_type",
+            "author",
+        )
+
+    def create(self, validated_data):
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
+            user = request.user
+            validated_data["author"] = user.id
+        
+        return super(CreateDegreeTypeDraftSerializer, self).create(validated_data)
 
 
 class ListDegreeTypeSerializer(serializers.ModelSerializer):
