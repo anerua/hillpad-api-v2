69526a51f35ce81b9e8db828930a22c6307008ef
Martins Anerua <aneruamartins@gmail.com>
2023-06-16 15:13:51 +0100

    feat: add create degreetypedraft url

--- academics/urls/degree_type.py
+++ academics/urls/degree_type.py
@@ -4,7 +4,7 @@ from academics import views
 
 
 urlpatterns = [
-    path("create", views.CreateDegreeTypeAPIView.as_view(), name="create_degree_type"),
+    path("create_draft", views.CreateDegreeTypeDraftAPIView.as_view(), name="create_degree_type_draft"),
     path("list", views.ListDegreeTypeAPIView.as_view(), name="list_degree_type"),
     path("detail/<int:pk>", views.DetailDegreeTypeAPIView.as_view(), name="detail_degree_type"),
     path("update/<int:pk>", views.UpdateDegreeTypeAPIView.as_view(), name="update_degree_type"),
