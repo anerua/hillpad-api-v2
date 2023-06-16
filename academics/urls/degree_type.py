dacd4aa3a232affd0391af2c219c4c70e4a1ba22
Martins Anerua <aneruamartins@gmail.com>
2023-06-16 16:23:36 +0100

    feat: add detail degreetypedraft url

--- academics/urls/degree_type.py
+++ academics/urls/degree_type.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("list", views.ListDegreeTypeAPIView.as_view(), name="list_degree_type"),
     path("list_draft", views.ListDegreeTypeDraftAPIView.as_view(), name="list_degree_type_draft"),
     path("detail/<int:pk>", views.DetailDegreeTypeAPIView.as_view(), name="detail_degree_type"),
+    path("detail_draft/<int:pk>", views.DetailDegreeTypeDraftAPIView.as_view(), name="detail_degree_type_draft"),
     path("update/<int:pk>", views.UpdateDegreeTypeAPIView.as_view(), name="update_degree_type"),
     path("delete/<int:pk>", views.DeleteDegreeTypeAPIView.as_view(), name="delete_degree_type"),
     path("publish/<int:pk>", views.PublishDegreeTypeAPIView.as_view(), name="publish_degree_type"),
