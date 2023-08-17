ff4122d1f2e7ae04e9ba8cd45ba09be05ab4a278
Martins Anerua <aneruamartins@gmail.com>
2023-08-17 10:05:50 +0100

    feat: supervisor can edit course before approval

--- academics/serializers/course.py
+++ academics/serializers/course.py
@@ -378,14 +378,83 @@ class SubmitCourseDraftSerializer(serializers.ModelSerializer):
 
 class ApproveCourseDraftSerializer(serializers.ModelSerializer):
 
+    start_month = serializers.IntegerField(min_value=0, max_value=12, write_only=True, required=False)
+    start_year = serializers.IntegerField(write_only=True, required=False)
+    deadline_month = serializers.IntegerField(min_value=0, max_value=12, write_only=True, required=False)
+    deadline_year = serializers.IntegerField(write_only=True, required=False)
+
     class Meta:
         model = CourseDraft
         fields = (
             "id",
+            "name",
+            "about",
+            "overview",
+            "duration",
+            "duration_base",
+            "start_month",
+            "start_year",
+            "deadline_month",
+            "deadline_year",
+            "course_dates",
+            "school",
+            "disciplines",
+            "tuition_fee",
+            "tuition_fee_base",
+            "tuition_currency",
+            "course_format",
+            "attendance",
+            "programme_type",
+            "degree_type",
+            "language",
+            "programme_structure",
+            "admission_requirements",
+            "official_programme_website",
             "status",
         )
+        extra_kwargs = {
+            "name": {"required": False},
+            "about": {"required": False},
+            "overview": {"required": False},
+            "duration": {"required": False},
+            "duration_base": {"required": False},
+            "start_month": {"required": False},
+            "start_year": {"required": False},
+            "deadline_month": {"required": False},
+            "deadline_year": {"required": False},
+            "course_dates": {"required": False},
+            "school": {"required": False},
+            "disciplines": {"required": False},
+            "tuition_fee": {"required": False},
+            "tuition_fee_base": {"required": False},
+            "tuition_fee_currency": {"required": False},
+            "course_format": {"required": False},
+            "attendance": {"required": False},
+            "programme_type": {"required": False},
+            "degree_type": {"required": False},
+            "language": {"required": False},
+            "programme_structure": {"required": False},
+            "admission_requirements": {"required": False},
+            "official_programme_website": {"required": False},
+        }
 
     def update(self, instance, validated_data):
+        course_dates = {}
+        if "start_month" in validated_data:
+            course_dates["start_month"] = validated_data["start_month"]
+            del validated_data["start_month"]
+        if "start_year" in validated_data:
+            course_dates["start_year"] = validated_data["start_year"]
+            del validated_data["start_year"]
+        if "deadline_month" in validated_data:
+            course_dates["deadline_month"] = validated_data["deadline_month"]
+            del validated_data["deadline_month"]
+        if "deadline_year" in validated_data:
+            course_dates["deadline_year"] = validated_data["deadline_year"]
+            del validated_data["deadline_year"]
+        if course_dates:
+            validated_data["course_dates"] = course_dates
+            
         validated_data["status"] = CourseDraft.APPROVED
         return super(ApproveCourseDraftSerializer, self).update(instance, validated_data)
 
