27c4a49c31870b0352a7aed6978b948af4e0b325
Martins Anerua <aneruamartins@gmail.com>
2023-09-14 09:16:27 +0100

    fix: country not paginated

--- academics/paginations.py
+++ academics/paginations.py
@@ -33,7 +33,7 @@ class SchoolDraftPagination(PageNumberPagination):
 
 class CountryPagination(PageNumberPagination):
 
-    page_size = 20
+    page_size = 1000
     page_size_query_param = "page_size"
 
 
